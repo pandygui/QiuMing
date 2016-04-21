7c55dcbc314ac009fb46a9c2d37fbe3fe9e70b6b
hxiaope <hxiaope@qq.com>
2016-04-21 19:46:53 +0800

    Java

---
+++ server/SoftProgram/src/com/softprogram/Post.java
@@ -0,0 +1,14 @@
+package com.softprogram;
+/*
+ * 帖子模块*/
+public class Post {
+	public void CreatePost(){}//创建帖子
+	public void viewPost(){}//查看帖子
+	public void modigyPost(){}//修改帖子
+	public void collectionPost(){}//收藏帖子
+	public void setlablePost(){}//设置帖子标签
+	public void likelablePost(){}//赞同帖子标签
+	public void likePost(){}//点赞帖子
+	public void deletePost(){}//删除帖子
+	public void reportPost(){}//举报帖子
+}
